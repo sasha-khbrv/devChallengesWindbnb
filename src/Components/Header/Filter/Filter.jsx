2b9d770be0e1a6b69d67244003af1f42500720c3
Aleksandra Khabarova <sasha.khbrv@gmail.com>
2021-01-22 15:46:55 +0300

    Made page responsive. Started to work with header

--- src/Components/Header/Filter/Filter.jsx
+++ src/Components/Header/Filter/Filter.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 function Filter() {
   return (
     <div className="filterContainer">
-      <div className="filterBtn-left">
+      <div className="filterBtn filterBtn-left">
         <button className="btn">Helsinki, Finland</button>
       </div>
-      <div className="filterBtn-center">
+      <div className="filterBtn filterBtn-center">
         <button className="btn btnPlaceholder">Add guests</button>
       </div>
-      <div className="filterBtn-right">
+      <div className="filterBtn filterBtn-right">
         <button className="btn btnIcon"><span className="material-icons">search</span></button>
       </div>      
     </div>
