3c0e8c7af121d459f4599e2844aca5f93bd5b714
Aleksandra Khabarova <sasha.khbrv@gmail.com>
2021-01-22 12:35:55 +0300

    Finish styling the first filter state (collapsed)

--- src/Components/Header/Filter/Filter.jsx
+++ src/Components/Header/Filter/Filter.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 function Filter() {
   return (
     <div className="filterContainer">
-      <button className="btn">Helsinki, Finland</button>
-      <button className="btn">Add guests</button>
-      <button className="btn"><span className="material-icons">search</span></button>
+      <div className="filterBtn-left">
+        <button className="btn">Helsinki, Finland</button>
+      </div>
+      <div className="filterBtn-center">
+        <button className="btn btnPlaceholder">Add guests</button>
+      </div>
+      <div className="filterBtn-right">
+        <button className="btn btnIcon"><span className="material-icons">search</span></button>
+      </div>      
     </div>
   );
 }
