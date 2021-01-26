d514872f30ea79210411c4403ec6a668a3fcfe6c
Aleksandra Khabarova <sasha.khbrv@gmail.com>
2021-01-26 12:32:48 +0300

    Added Formik, Yup. Fixed small bugs.
    The footer.scss file was lost. Fixed the output of the number of stays

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Card from './Card/Card';
 
 function Cards({stays}) {
-  const staysAmount = (stays.length === 1) ? `${stays.length} stay` : `${stays.length} stays`
-  const cardArray = stays.map(item => <Card key={item.id} 
-                                            photo={item.photo}
-                                            alt={item.alt} 
-                                            isSuperHost={item.isSuperHost} 
-                                            tags={item.tags} 
-                                            rating={item.rating} 
-                                            title={item.title} />)
+  const cardArray = stays.map(item => 
+    <Card key={item.id} 
+          photo={item.photo}
+          alt={item.alt} 
+          isSuperHost={item.isSuperHost} 
+          tags={item.tags} 
+          rating={item.rating} 
+          title={item.title} />
+  );
+  let staysAmount = (cardArray.length === 1) ? `${cardArray.length} stay` : `${cardArray.length} stays`
   return (
     <main className="contentContainer">
       <div className="contentHeader">
